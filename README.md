6fc386edfcdb1042bb7ef7c576a5bf6d38ab99e5
dngst <dngst@protonmail.com>
2024-09-03 03:51:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Trophy Room
+
+> WIP
+
+# Install
+
+$ bundle
+
+# Usage
+
+$ ruby extract_data.rb
+
---
+++ extract_data.rb
@@ -0,0 +1,25 @@
+require 'musicbrainz'
+
+MusicBrainz.configure do |c|
+  # Application identity (required)
+  c.app_name = "trophyroom"
+  c.app_version = "1.0"
+  c.contact = "eddieatsenga@protonmail.com"
+end
+
+
+def find_album_mbid(artist, album_name)
+  results = MusicBrainz::ReleaseGroup.search(artist, album_name, 'Album')
+  release_group = results.first
+
+  if release_group
+    "MBID for album '#{album_name}': #{release_group[:id]}"
+  else
+    nil
+  end
+end
+
+artist = 'Aretha Franklin'
+album_name = 'I Never Loved a Man the Way I Love You'
+
+puts find_album_mbid(artist, album_name)
