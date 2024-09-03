add89dfdc21a4419550de05ab0bea34934ac48b2
dngst <dngst@protonmail.com>
2024-09-03 05:36:44 +0300

    Extract album covers

--- extract_data.rb
+++ extract_data.rb
@@ -1,5 +1,7 @@
 require 'csv'
+require 'json'
 require 'musicbrainz'
+require 'httparty'
 
 MusicBrainz.configure do |c|
   # Application identity (required)
@@ -13,17 +15,36 @@ def find_album_mbid(artist, album_name)
   release_group = results.first
 
   if release_group
-    "MBID for album '#{album_name}': #{release_group[:id]}"
-  else
-    "No MBID found for album '#{album_name}' by #{artist}"
+    release_group[:id]
   end
 end
 
+def get_cover_art(mbid)
+  response = HTTParty.get("https://coverartarchive.org/release-group/#{mbid}")
+  response.parsed_response['images'].first['image'] if response.parsed_response['images']
+end
+
 csv_file_path = 'albums.csv'
+album_data = []
 
 CSV.foreach(csv_file_path, headers: true) do |row|
   artist = row['Artist']
   album_name = row['Album']
-  puts find_album_mbid(artist, album_name)
+  mbid = find_album_mbid(artist, album_name)
+  cover_art_url = get_cover_art(mbid)
+
+  if cover_art_url
+    album_data << {
+      "artist" => artist,
+      "album" => album_name,
+      "image" => cover_art_url
+    }
+  end
 end
+
+File.open("album_covers.json", "w") do |file|
+  file.write(JSON.pretty_generate(album_data))
+end
+
+puts "Done."
 
