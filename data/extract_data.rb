d0b70ef7f49a9c96a92f19cf385b5a80c25e8eb5
dngst <dngst@protonmail.com>
2024-09-04 03:41:47 +0300

    Update albums

--- data/extract_data.rb
+++ data/extract_data.rb
@@ -7,7 +7,7 @@ def find_album_mbid(artist, album_name)
   url = "https://musicbrainz.org/ws/2/release-group/?query=artist:#{URI.encode_www_form_component(artist)}%20AND%20release:#{URI.encode_www_form_component(album_name)}&fmt=json"
   response = HTTParty.get(url, headers: { 'User-Agent' => 'trophyroom/1.0 (eddieatsenga@protonmail.com)' })
 
-  response.code == 200 && response['release-groups'] && !response['release-groups'].empty? && response['release-groups'].first['id']
+  response['release-groups'].first['id']
 end
 
 def get_cover_art(mbid)
@@ -21,7 +21,7 @@ json_file_path = 'album_covers.json'
 album_data = File.exist?(json_file_path) ? JSON.parse(File.read(json_file_path)) : []
 
 no_of_albums = CSV.foreach(csv_file_path, headers: true).count
-progressbar = ProgressBar.create(title: "Processing albums", total: no_of_albums)
+progressbar = ProgressBar.create(title: "Fetching albums", total: no_of_albums,format: "%t: |%B| %p%%")
 
 CSV.foreach(csv_file_path, headers: true) do |row|
   artist = row['Artist']
